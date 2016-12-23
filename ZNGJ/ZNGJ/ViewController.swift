8ab6a856ba1d6c0ec7f533bdd341371134174479
enli <enli@microstrategy.com>
2016-12-23 11:46:44 +0800

    new project

---
+++ ZNGJ/ZNGJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZNGJ
+//
+//  Created by en li on 16/12/23.
+//  Copyright © 2016年 en li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
