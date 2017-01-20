fb7eb18ff1ce6c40a7d884a2465bd9f2521db208
enli <enli@microstrategy.com>
2017-01-20 11:26:17 +0800

    添加自动登录和记住密码功能

--- ZNGJ/ZNGJ/UI/LoginViewController.swift
+++ ZNGJ/ZNGJ/UI/LoginViewController.swift
@@ -11,11 +11,17 @@ import UserNotifications
 
 class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 {
-	
 	@IBOutlet weak var cellphone: UITextField!
 	@IBOutlet weak var password: UITextField!
-	
 	var currTextField: UITextField!
+
+	@IBOutlet weak var autoLoginCheckBox: UIButton!
+	@IBOutlet weak var rememberPWCheckBox: UIButton!
+	
+	// 用户名密码默认全部记录在userdefault standard中
+	var rememberPasswordCheck: Bool = true
+	var autoLoginCheck: Bool = true
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -25,6 +31,59 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 		
 		// 点击空白取消键盘
 		self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.handleSingleTap(_:))))
+		
+		// Try to login directly
+		self.autoLogin()
+	}
+	
+	func autoLogin()
+	{
+		// 从user default standard中读取用户名密码
+		let username:String? = UserDefaults.standard.string(forKey: "username")
+		guard username != nil else {
+			return
+		}
+		let password:String? = UserDefaults.standard.string(forKey: "password")
+		guard password != nil else {
+			return
+		}
+		
+		// 从user default standard中读取用户设置, 进行UI初始化
+		let rememberPassword:String? = UserDefaults.standard.string(forKey: "rememberPassword")
+		if rememberPassword == nil {
+			self.rememberPasswordCheck = true
+		} else {
+			self.rememberPasswordCheck = rememberPassword! == "TRUE" ? true : false
+		}
+		if self.rememberPasswordCheck {
+			self.cellphone.text = username
+			self.password.text = password
+		}
+		let autoLogin:String? = UserDefaults.standard.string(forKey: "autoLogin")
+		if autoLogin == nil {
+			self.autoLoginCheck = true
+		} else {
+			self.autoLoginCheck = autoLogin! == "TRUE" ? true : false
+		}
+		self.updateAutoLoginUI()
+		
+		if self.autoLoginCheck {
+			self.doLogin(username: username!, password: password!)
+		}
+	}
+	
+	func updateAutoLoginUI() {
+		if self.autoLoginCheck {
+			self.autoLoginCheckBox.setImage(UIImage.init(named: "box-checked"), for: UIControlState.normal)
+		} else {
+			self.autoLoginCheckBox.setImage(UIImage.init(named: "box-empty"), for: UIControlState.normal)
+		}
+		
+		if self.rememberPasswordCheck {
+			self.rememberPWCheckBox.setImage(UIImage.init(named: "box-checked"), for: UIControlState.normal)
+		} else {
+			self.rememberPWCheckBox.setImage(UIImage.init(named: "box-empty"), for: UIControlState.normal)
+		}
 	}
 	
 	func setupTextField()
@@ -34,16 +93,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 		self.password.delegate = self
 		
 		// 返回按钮设置
-		self.cellphone.returnKeyType = UIReturnKeyType.done;
-		self.password.returnKeyType = UIReturnKeyType.done;
+		self.cellphone.returnKeyType = UIReturnKeyType.done
+		self.password.returnKeyType = UIReturnKeyType.done
 		
 		// 键盘格式
-		self.cellphone.keyboardType = UIKeyboardType.phonePad;
-		self.password.keyboardType = UIKeyboardType.phonePad;
+		self.cellphone.keyboardType = UIKeyboardType.phonePad
+		self.password.keyboardType = UIKeyboardType.phonePad
 		
 		// 开启一键删除操作
-		self.cellphone.clearsOnBeginEditing = true;
-		self.password.clearsOnBeginEditing = true;
+		self.password.clearsOnBeginEditing = true
+		
+		// 密码模式
+		self.password.isSecureTextEntry = true
 	}
 	
 	//TextField - Delegate
@@ -75,14 +136,18 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 			self.showAlert(title: "登录失败", message: "用户名或者密码不能为空!")
 			return
 		}
-		
+	
 		// 2. 向服务器发送登录请求
+		self.doLogin(username: self.cellphone.text!, password: self.password.text!)
+	}
+	
+	func doLogin(username:String, password:String)
+	{
 		let request:ZNGJRequest = ZNGJRequestManager.shared().createRequest(ENUM_REQUEST_LOGIN)
-		let params:Dictionary<String, String> = ["cellphone":self.cellphone.text!, "password":self.password.text!]
+		let params:Dictionary<String, String> = ["cellphone":username, "password":password]
 		request.params = params
 		request.handler = self
 		request.start()
-
 	}
 	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -100,8 +165,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 				self.registerDeviceForPushNotification()
 				
 				// 2. 更新用户名，密码到本地数据存储
-				UserDefaults.standard.set(self.cellphone.text!, forKey: "username")
-				UserDefaults.standard.set(self.password.text!, forKey: "password")
+				if self.rememberPasswordCheck || self.autoLoginCheck {
+					UserDefaults.standard.set(self.cellphone.text!, forKey: "username")
+					UserDefaults.standard.set(self.password.text!, forKey: "password")
+					UserDefaults.standard.set(self.autoLoginCheck ? "TRUE" : "FALSE", forKey: "autoLogin")
+					UserDefaults.standard.set(self.rememberPasswordCheck ? "TRUE" : "FALSE", forKey: "rememberPassword")
+				}
 				
 				// 3. 到客户管理界面
 				let mainTBVC = self.storyboard!.instantiateViewController(withIdentifier: "MainTBViewController")
@@ -113,7 +182,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 		}
 	}
 	func onFailure(_ error: Error!) {
-		showAlert(title: "请求失败", message: "网络请求失败，请重试!")
+		showAlert(title: "请求失败", message: "登录请求失败，请重试!")
 	}
 	
 	func registerDeviceForPushNotification()
@@ -140,10 +209,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate, RequestHandler
 	
 	// 自动登录
 	@IBAction func checkAutoLogin(_ sender: UIButton) {
+		self.autoLoginCheck = !self.autoLoginCheck
+		self.updateAutoLoginUI()
 	}
 	
 	// 记住密码
 	@IBAction func checkRecordPassword(_ sender: UIButton) {
+		self.rememberPasswordCheck = !self.rememberPasswordCheck
+		self.updateAutoLoginUI()
 	}
 	
 	// adjust view height when keyboard show
