e765baa479451d8bc3404025e621f619981b5b38
enli <enli@microstrategy.com>
2017-01-23 20:22:48 +0800

    添加订单详情UI

--- ZNGJ/ZNGJ/Model/Order.swift
+++ ZNGJ/ZNGJ/Model/Order.swift
@@ -20,6 +20,14 @@ class Order{
 	}
     
     var orderId: Int = 0
-    var orderAddress: String = ""
+    var orderAddress: String = "初始地址"
+	
+	var orderNo: String = "初始编号"
+	var orderStatus: String = "未接单"
+	var orderBookingDate: String = "初始时间"
+	var orderContact: String = "初始联系人"
+	var orderCellPhone: String = "初始手机号"
+	var orderReservedDate: String = "初始时间"
+	var orderProduction: String = "初始类目"
     
 }
