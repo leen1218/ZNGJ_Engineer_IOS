ce87f38d94eef58c51fc433491f77196e02c5e94
enli <enli@microstrategy.com>
2017-03-15 15:29:37 +0800

    添加app icon和修改app名称，添加工程师认证UI

---
+++ ZNGJ/ZNGJ/UI/AuthtificationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthtificationViewController.swift
+//  ZNGJ
+//
+//  Created by en li on 17/3/15.
+//  Copyright © 2017年 en li. All rights reserved.
+//
+
+import Foundation
+
+class AuthtificationViewController: ViewController {
+	
+}
